b78d1a19ce4edcef20aa32b7d887ad8468161d59
Gaspard Shen <syuyang@gmail.com>
2017-09-12 19:41:29 +0000

    Correct the full connect of the model

--- model.py
+++ model.py
@@ -28,6 +28,7 @@ for image, measurement in zip(images, measurements):
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
+print(X_train.shape, y_train.shape)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
@@ -45,30 +46,10 @@ model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, verbose = 2, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
 
 model.save('model.h5')
-
-from keras.models import Model
-import matplotlib.pyplot as plt
-
-history_object = model.fit_generator(train_generator, samples_per_epoch =
-    len(train_samples), validation_data = 
-    validation_generator,
-    nb_val_samples = len(validation_samples), 
-    nb_epoch=5, verbose=1)
-
-### print the keys contained in the history object
-print(history_object.history.keys())
-
-### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
